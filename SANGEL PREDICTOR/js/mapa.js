9c19efd44e671b1bd041a80f96d903592c46ed1a
darlallano23-max <darlallano23@gmail.com>
2026-06-14 23:56:03 -0500

    Delete SANGEL PREDICTOR directory

--- SANGEL PREDICTOR/js/mapa.js
+++
@@ -1,9 +0,0 @@
-// Inicializar el mapa centrado en coordenadas generales
-const map = L.map('map').setView([20, 0], 2);
-
-// Cargar capa de mapa base de código abierto (OpenStreetMap)
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '© OpenStreetMap contributors'
-}).addTo(map);
-
-console.log("Módulo del mapa cargado correctamente.");
